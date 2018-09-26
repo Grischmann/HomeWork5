11f8616875b3ced26f3f361d73246cb46b80bcd3
Grischmann <43209362+Grischmann@users.noreply.github.com>
2018-09-26 14:24:56 +0300

    Update AppTest.kt

--- src/test/kotlin/ru/yamoney/test/AppTest.kt
+++ src/test/kotlin/ru/yamoney/test/AppTest.kt
@@ -13,36 +13,42 @@ class AppTest {
     @Test
     fun paymentTest() {
         val sum = BigDecimal("33")
-        val depositOperation = Deposit("Вася", sum)
-        val beforeBalance = billing.getUserBalance("Вася")
+        val Payer = "Вася"
+        val shopID = "4454"
+        val depositOperation = Deposit(Payer, sum)
+        println("Пополняем Васе счёт - " + depositOperation)
+        val beforeBalance = billing.getUserBalance(Payer)
         billing.addOperation(depositOperation)                              //Пополнаяем Счет Васи на 33
-        val paymentOperation = Payment("Вася", sum, "4454")
-        val afterBalance = billing.getUserBalance("Вася")
-        billing.addOperation(paymentOperation)                              //Вася платит за Твист и Рок&Ролл
-        println("1 Пополняем Васе счёт - " + depositOperation)
-        println("2 Проверяем Васин баланс до = " + beforeBalance)
-        println("3 Вася платит в магазин за Твист и РокНРолл - " + paymentOperation)
-        println("4 Проверим Васин Баланс после платежа = " + billing.getUserBalance("Вася"))
+        println("Проверяем Васин баланс до = " + beforeBalance)
 
-        assertThat("5 Проверяем, что Вася действительно заплатил и баланс равен ожидаемому",
+        val paymentOperation = Payment(Payer, sum, shopID)
+        println("Вася платит в магазин за Твист и РокНРолл - " + paymentOperation)
+
+        val afterBalance = billing.getUserBalance(Payer)
+        println("Проверим Васин Баланс после платежа = " + billing.getUserBalance(Payer))
+
+        billing.addOperation(paymentOperation)                              //Вася платит за Твист и Рок&Ролл
+        assertThat("Проверяем, что Вася действительно заплатил и баланс равен ожидаемому",
                 afterBalance, Matchers.comparesEqualTo(beforeBalance + sum))
     }
 
     @Test
-    fun balanceTest() {
+      fun balanceTest() {
         val depositSum = BigDecimal("100")
-        val user = "Depositmann"
-        val beforeBalance = billing.getUserBalance(user)
-        val depositOperation = Deposit(user, depositSum)
-        billing.addOperation(depositOperation)
-
-        val paymentOperation = Payment(user, depositSum, "4454")
-        billing.addOperation(paymentOperation)
-        val afterOperation = billing.getUserBalance(user)
-
+        val payer = "Depositmann"
+        val shopId = "4454"
+        val beforeBalance = billing.getUserBalance(payer)
         println(beforeBalance)
+
+        val depositOperation = Deposit(payer, depositSum)
+        billing.addOperation(depositOperation)
         println(depositOperation)
+
+        val paymentOperation = Payment(payer, depositSum, shopId)
+        billing.addOperation(paymentOperation)
         println(paymentOperation)
+
+        val afterOperation = billing.getUserBalance(payer)
         println(afterOperation)
 
         assertThat("Проверяем, что баланс Depositmann остался как и прежде (до пополнения и затем списания)",
@@ -52,41 +58,21 @@ class AppTest {
     @Test
     fun shopInfoTest() {
         val user = "Kaufmann"
-        val kaufSumEins = BigDecimal("100")
-        val kaufSumZwei = BigDecimal("100")
+        val price1 = BigDecimal("100")
+        val price2 = BigDecimal("200")
+        val shopId= "4454"
         listBefore = billing.addOperation(paymentOperation)
-        val paymentOperation = Payment(user, kaufSumEins, "4454")
-        val paymentOperation2 = Payment(user, kaufSumZwei, "4454")
-        billing.addOperation(paymentOperation2)
-    // Payment 1
+        val paymentOperation = Payment(user, price1, shopId)
         println("Платеж №1 " + paymentOperation)
-    // Payment 2
-        println("Платеж №2 " + paymentOperation2)
-    // shopInfo
-        listAfter= billing.getShopIdOperations("4454"))
+        
+        billing.addOperation(paymentOperation2)
+        val paymentOperation2 = Payment(user, price2, shopId)
+        println("Платеж №2 " + paymentOperation2)   
+  
+        listAfter= billing.getShopIdOperations(shopId))
 
         assertThat("Проверяем, что в листе операций магазина стало на две записи больше ",
                 listAfter.size - listBefore.size .equalTo(2))
-
-
-
-
-
-        // val list = billing.getShopIdOperations("4454"))
-
-
-        //val shopInfo = mutableListOf<String>()
-
-                //.add(toString(paymentOperation))
-
-        //billing.getShopIdOperations("4454")
-
-       //billing.addOperation(shopInfo(shopInfo))
-        //println("Смотрим в лист операций магазина " + shopInfo )
-    // assert that shop has 2 payments
-//        assertThat("Проверяем, что в магазине присутствуют две записи о платежах",
-//                shopInfo("4454") )
-    }
 
     @Test
     fun payment() {
